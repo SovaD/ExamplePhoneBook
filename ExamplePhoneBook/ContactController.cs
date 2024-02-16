fd058fc0effa1c8d6d5a18e19c82451fdfe6d335
Sova <bakasovap@mail.ru>
2024-02-16 12:27:23 +0600

    Добавьте файлы проекта.

---
+++ ExamplePhoneBook/ContactController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace ExamplePhoneBook
+{
+    internal class ContactController
+    {
+        private ContactManager contactManager;
+        private Form1 view; // Замените Form1 на название вашей формы
+
+        public ContactController(Form1 view)
+        {
+            this.view = view;
+            contactManager = new ContactManager();
+            WireUpEvents();
+        }
+
+        private void WireUpEvents()
+        {
+            view.Load += View_Load;
+            view.bAdd.Click += Btn_Click;
+            view.DataGrid.CellClick += dataGrid_CellClick;
+            // Другие события элементов управления
+        }
+
+        private void View_Load(object sender, EventArgs e)
+        {
+            // При загрузке формы загрузите контакты из модели и отобразите их в DataGridView
+            view.DataGrid.DataSource = contactManager.GetContacts();
+        }
+
+        public void Btn_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            switch (button.Text.ToLower())
+            {
+                case "add":
+                    // При нажатии кнопки "Добавить контакт" получите данные из TextBox
+                    // и добавьте новый контакт в модель
+                    add();
+                    break;
+                case "edit":
+                    break;
+                case "delete":
+                    break;
+                case "cancel":
+                    clearTextBoxes();
+                    break;
+                case "find":
+                    find(view.vFind.Text);
+                    break;
+            }
+
+        }
+
+        Contact newContact;
+
+        bool checkValues()
+        {
+            if (view.vName.Text != "")
+            {
+                newContact = new Contact
+                {
+                    Name = view.vName.Text,
+                    Email = view.vEmail.Text,
+                    PhoneNumber = view.vPhone.Text,
+                    Group = view.vGroup.Text,
+                };
+                return true;
+            }
+            else { return false; }
+        }
+        // Другие методы для обработки событий и взаимодействия с моделью
+        private void add()
+        {
+            if (checkValues())
+            {
+                contactManager.AddContact(newContact);
+                //view.DataGrid.DataSource = contactManager.GetContacts();
+                // view.DataGrid.Refresh();// Обновите список контактов на форме
+                refreshDataGrid(contactManager.GetContacts());
+            }
+
+        }
+
+        void clearTextBoxes()
+        {
+            view.vGroupBox1.Controls.OfType<Button>().ToList().ForEach(x => x.Text = "");
+            view.vGroup.SelectedIndex = 0;
+        }
+
+        internal void find(string text)
+        {
+            List<Contact> res = contactManager.GetContacts(text);
+            refreshDataGrid(res);
+        }
+        void refreshDataGrid(List<Contact> source)
+        {
+            view.DataGrid.DataSource = source;
+            var m = view.DataGrid.GetType().GetMethod("OnDataSourceChanged",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+            m.Invoke(view.DataGrid, new object[] { EventArgs.Empty });
+        }
+        private void dataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = int.Parse(view.DataGrid.CurrentRow.Cells[0].Value.ToString());
+            Contact contact = contactManager.GetContact(id);
+            fillTextBoxes(contact);
+        }
+        void fillTextBoxes(Contact contact)
+        {
+            if (!string.IsNullOrEmpty(contact.Name))
+            {
+                view.vName.Text = contact.Name;
+                view.vEmail.Text = contact.Email;
+                view.vPhone.Text = contact.PhoneNumber;
+                view.vGroup.Text = contact.Group;
+            }
+        }
+    }
+
+}
---
+++ ExamplePhoneBook/ContactManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamplePhoneBook
+{
+    public class ContactManager
+    {
+        private List<Contact> contacts;
+        public ContactManager()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Sort((x, y) => x.Id.CompareTo(y.Id));
+            contact.Id = contacts.Count() == 0 ? 1 : contacts.Last().Id + 1;
+            contacts.Add(contact);
+        }
+        public bool EditContact(int id, Contact contact)
+        {
+            Contact old = contacts.Find(x => x.Id == id);
+            try
+            {
+                contacts.Remove(contact);
+                contacts.Add(contact);
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool RemoveContact(Contact contact)
+        {
+            try
+            {
+                contacts.Remove(contact);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Contact GetContact(int id)
+        {
+            return contacts.Find(x => x.Id == id);
+        }
+
+        public List<Contact> GetContacts()
+        {
+            return contacts;
+        }
+        public List<Contact> GetContacts(string value)
+        {
+            value = value.ToLower();
+            return contacts.FindAll(x => x.Name.ToLower().Contains(value)
+            || x.PhoneNumber.Contains(value)
+            || x.Email.Contains(value)
+            || x.Group.ToLower().Contains(value));
+        }
+    }
+
+}
